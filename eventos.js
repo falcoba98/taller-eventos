944a297fa33d65bf5435a2b954565cfb6720597b
falcoba98 <falcoba98@gmail.com>
2024-08-16 15:49:05 -0300

    Creacion js y modificacion index HTML
    Se creó js para generar alerta al presionar botón dentro del div creado en HTML que contiene el botón. Se tuvo que eliminar función de click en HTML para que funcione el js.

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let button = document.getElementById("button");
+
+    button.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
